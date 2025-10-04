53d0e71854ee4ae0857b2c0b7812d1efd6c89776
emskypl <mateusz.emsky@gmail.com>
2025-10-04 20:56:09 +0200

    Added Cors for localhost, app.tsx fetch data by axios and Material IU added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,29 @@
+import { List, ListItem, ListItemText, Typography } from '@mui/material'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 function App() {
-	return <h3>Health App</h3>
+	const [activities, setActivities] = useState<Activity[]>([])
+
+	useEffect(() => {
+		axios
+			.get<Activity[]>('https://localhost:5002/api/activities')
+			.then(response => setActivities(response.data))
+			.catch(error => console.error('Error fetching activities:', error))
+	}, [])
+
+	return (
+		<>
+			<Typography variant='h3'>Health App</Typography>
+			<List>
+				{activities.map(activity => (
+					<ListItem key={activity.id}>
+						<ListItemText>{activity.title}</ListItemText>
+					</ListItem>
+				))}
+			</List>
+		</>
+	)
 }
 
 export default App
